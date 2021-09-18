8f926551bc8eb2fad90404aa2efac16c43149a02
kevinkerker <90988320+kevinkerker@users.noreply.github.com>
2021-09-18 23:21:58 +0200

    Updated for pip version > 10.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in gi2et/__init__.py
@@ -21,6 +21,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
+	install_requires=[str(ir.requirement) for ir in requirements],
 	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
